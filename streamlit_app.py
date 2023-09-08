3bc2779396212009314cc96faf464dfab2af4c4c
Smita Singh <119648166+singhsmita@users.noreply.github.com>
2023-09-08 12:54:48 -0400

    Update streamlit_app.py
    updating code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,28 @@
-import streamlit
+//import streamlit
 
-streamlit.title('My parents New Healthy Diner.')
+//streamlit.title('My parents New Healthy Diner.')
 
+//streamlit.markdown("## Subheading 1")
+
+//streamlit.write("Breakfast Menu")
+
+//text_lines = ["Omega 3 & Blueberry Oatmeal" , "Kale ,Spinach & Rocket Smoothie" , "Hard Boiled Free-Range Egg" ]
+//streamlit.write(text_lines)
+
+
+import streamlit as streamlit
+
+# Set the title
+streamlit.title("My parents' New Healthy Diner.")
+
+# Subheading
 streamlit.markdown("## Subheading 1")
 
+# Display the breakfast menu
 streamlit.write("Breakfast Menu")
 
-text_lines = ["Omega 3 & Blueberry Oatmeal" , "Kale ,Spinach & Rocket Smoothie" , "Hard Boiled Free-Range Egg" ]
+# List of menu items
+text_lines = ["Omega 3 & Blueberry Oatmeal", "Kale, Spinach & Rocket Smoothie", "Hard Boiled Free-Range Egg"]
+
+# Display the menu items as a list
 streamlit.write(text_lines)
