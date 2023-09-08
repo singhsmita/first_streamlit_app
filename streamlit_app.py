e7155586a2bab9a2e282f62d95ebd1cdef6b65f7
Smita Singh <119648166+singhsmita@users.noreply.github.com>
2023-09-08 16:57:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,13 @@ st.title('My parents New Healthy Diner.')
 
 st.subheader("Breakfast Menu")
 
-text_lines = ["Omega 3 & Blueberry Oatmeal" , "Kale ,Spinach & Rocket Smoothie" , "Hard Boiled Free-Range Egg" ]
-st.write(text_lines)
+multiline_text = """
+Omega 3 & Blueberry Oatmeal
+Kale, Spinach & Rocket Smoothie
+Hard Boiled Free-Range Egg
+"""
+
+st.write(multiline_text)
 
 
 
