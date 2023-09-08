f4ff8c6403eaa97b495efad303cd51f1f38f5ba8
Smita Singh <119648166+singhsmita@users.noreply.github.com>
2023-09-08 16:51:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
- import streamlit as st
+import streamlit as st
 
-  st.title('My parents New Healthy Diner.')
+st.title('My parents New Healthy Diner.')
 
- st.subheader("Breakfast Menu")
+st.subheader("Breakfast Menu")
 
- text_lines = ["Omega 3 & Blueberry Oatmeal" , "Kale ,Spinach & Rocket Smoothie" , "Hard Boiled Free-Range Egg" ]
- st.write(text_lines)
+text_lines = ["Omega 3 & Blueberry Oatmeal" , "Kale ,Spinach & Rocket Smoothie" , "Hard Boiled Free-Range Egg" ]
+st.write(text_lines)
 
 
 
