30496419999d4b6fa48a780c77e6f1416fc8230e
Smita Singh <119648166+singhsmita@users.noreply.github.com>
2023-09-08 12:56:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,13 @@
-//import streamlit
+# import streamlit
 
-//streamlit.title('My parents New Healthy Diner.')
+# streamlit.title('My parents New Healthy Diner.')
 
-//streamlit.markdown("## Subheading 1")
+# streamlit.markdown("## Subheading 1")
 
-//streamlit.write("Breakfast Menu")
+# streamlit.write("Breakfast Menu")
 
-//text_lines = ["Omega 3 & Blueberry Oatmeal" , "Kale ,Spinach & Rocket Smoothie" , "Hard Boiled Free-Range Egg" ]
-//streamlit.write(text_lines)
+# text_lines = ["Omega 3 & Blueberry Oatmeal" , "Kale ,Spinach & Rocket Smoothie" , "Hard Boiled Free-Range Egg" ]
+# streamlit.write(text_lines)
 
 
 import streamlit as streamlit
