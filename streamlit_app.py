59d94267ea18bc73026d58900a4fb7665acbbef5
Smita Singh <119648166+singhsmita@users.noreply.github.com>
2023-09-08 13:20:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,13 @@
-# import streamlit
+ import streamlit as st
 
-# streamlit.title('My parents New Healthy Diner.')
+  st.title('My parents New Healthy Diner.')
 
-# streamlit.markdown("## Subheading 1")
+ 
 
-# streamlit.write("Breakfast Menu")
+ st.subheader("Breakfast Menu")
 
-# text_lines = ["Omega 3 & Blueberry Oatmeal" , "Kale ,Spinach & Rocket Smoothie" , "Hard Boiled Free-Range Egg" ]
-# streamlit.write(text_lines)
+ text_lines = ["Omega 3 & Blueberry Oatmeal" , "Kale ,Spinach & Rocket Smoothie" , "Hard Boiled Free-Range Egg" ]
+ st.write(text_lines)
 
 
-import streamlit as streamlit
 
-# Set the title
-streamlit.title("My parents' New Healthy Diner.")
-
-# Subheading
-# streamlit.markdown("## Subheading 1")
-
-# Display the breakfast menu
-streamlit.write("Breakfast Menu")
-
-# List of menu items
-text_lines = ["Omega 3 & Blueberry Oatmeal", "Kale, Spinach & Rocket Smoothie", "Hard Boiled Free-Range Egg"]
-
-# Display the menu items as a list
-streamlit.write(text_lines)
