55bd83919ff9df7d354bf2bf9d43d7c0c00b3eb6
Smita Singh <119648166+singhsmita@users.noreply.github.com>
2023-09-14 13:48:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ st.text(fruityvice_response.json())
 
 
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 st.dataframe(fruityvice_normalized)
 
