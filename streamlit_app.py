a3a006ae9b0b588d2194c965b85b1790d2a5acc1
Smita Singh <119648166+singhsmita@users.noreply.github.com>
2023-09-14 18:18:14 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,12 +48,12 @@ my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 st.header("The fruit load conatins:")
 #st.text("The fruit load list contains:")
 #st.text("Hello from Snowflake:")
 #st.text(my_data_row)
-st.dataframe(my_data_row)
+st.dataframe(my_data_rows)
 
 
 
