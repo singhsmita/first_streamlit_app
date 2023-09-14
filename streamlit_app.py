7fed2b71fa47250b873672e5287da065c8047fed
Smita Singh <119648166+singhsmita@users.noreply.github.com>
2023-09-14 13:43:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ st.dataframe(fruits_to_show)
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+st.text(fruityvice_response)
 
 
 
